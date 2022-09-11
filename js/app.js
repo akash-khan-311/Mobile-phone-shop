bad26d6a5a3d63dd4033757ea17768017262618b
Akash Khan <akashcc48@gmail.com>
2022-09-11 21:12:46 +0600

    UI Design And Search function Added

---
+++ js/app.js
@@ -0,0 +1,8 @@
+//  variable declearation
+const searchFeild = document.getElementById('search-feild')
+
+// Search Phone
+const searchPhone = () => {
+    const searchText = searchFeild.value;
+    searchFeild.value = '';
+}
